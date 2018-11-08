d377a5e8c1e5b3ceda59478fdc95aeb4792589a6
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2018-11-07 22:03:27 -0800

    Implemented the BONUS requirements: favorites section and offset feature.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# GifTastic
+# Gif Tastic
+
+## Introduction
+Web page enables users to load and view images of various animals.
+GIF images can be animated on demand. Images can be added to favorites. New animal topics can be added as you go.
+
+## Seach
+On the top right you can see the "Add an animal" form. Type an animal name and search for it. New animal button will be added to the head of the page. Use these new buttons to load new set of images about this animal.
+
+## Load Images
+Each time you click on the animal button page loads new set of 10 images. Images are stacked, so newly loaded images will always be on the top, while older images will scroll down.
+Click on the image will start or stop the animation.
+There's a plus button on the top right side of each image. It will add the image to the favorites section.
+
+## Favorites
+You can find the favorites section on the right side of the screen. Refresh will preserve your favorites. You have an option to remove images from favorites by clicking the X button that's on the top right of the image.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,17 @@
-// UI Part
-window.onload = function() {
+// Initialization logic goes here
+window.onload = function()
+{
+    var list = JSON.parse(localStorage.getItem("favorites"));
+    if (!Array.isArray(list)) {
+      list = [];
+    }
 
+    updateFavorites(list);
+
+    // Initial set of animals to be populated on the page.
     var animals = ["wolf", "owl", "shark", "peacock", "hippopotamus", "chicken", "anemon",
-    "whale", "parrot", "mountain beaver", "duck", "butterfly"];
+                    "whale", "parrot", "mountain beaver", "duck", "butterfly"];
+    
     for(var i = 0; i < animals.length; ++ i)
     {
         displayButton(animals[i]);
@@ -10,7 +19,13 @@ window.onload = function() {
 
     $("#btn-submit").on("click", function(){
         var name = $("#add").val();
-        displayButton(name);  
+        if(name != "" && !animals.includes(name))
+        {
+            displayButton(name);
+        }
+
+        animals.push(name);
+        $("#add").val("");
     })
     
     $(document).on("click", ".animal-btn", displayAnimalImages);
@@ -30,24 +45,48 @@ window.onload = function() {
         }
     });
 
+    $(document).on("click", ".add-favorite", function() {
+       
+        var img = $(this).parents(".img-wrapper").find(".img-unit");
+        list.push(img.attr("animated"));
+  
+        updateFavorites(list);
+        localStorage.setItem("favorites", JSON.stringify(list));
+        
+    });
+  
+    $(document).on("click", ".btn-delete", function() {
+        var number = $(this).attr("delete");
+  
+        list.splice(number, 1);
+        updateFavorites(list);
+        localStorage.setItem("favorites", JSON.stringify(list));
+    });
 }
 
+// Adds a new animal button to the page.
 function displayButton(name)
 {
     var newButton = $("<button>");
     newButton.addClass("animal-btn");
     newButton.attr("data-animal", name);
+    newButton.attr("offset", 0);
     newButton.text(name);
     $("#buttons").append(newButton);
 }
 
-function displayAnimalImages(){
+// Loads images from GIPHY, shows them on the page, deals with ratings and enables "Add To Favorites" button.
+//  Offset concept is handled here - each time given button is clicked, function loads new set of images from the new offset.
+function displayAnimalImages()
+{
     var animal = $(this).attr("data-animal");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NjeA3euYga2SrEIhVjtxb335fzPFo6Zq&q="+ animal +"&limit=10&offset=0&rating=&lang=en";
-    
+    var offset = parseInt($(this).attr("offset"), 10);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NjeA3euYga2SrEIhVjtxb335fzPFo6Zq&q=" + animal + "&limit=10&offset=" +  offset * 10 + "&rating=&lang=en";
+    offset ++;
+    $(this).attr("offset", offset);
     $.ajax({
-    url: queryURL,
-    method: "GET"
+        url: queryURL,
+        method: "GET"
     }).then(function(response) {
         console.log(response);
 
@@ -66,10 +105,32 @@ function displayAnimalImages(){
             image.attr("animated", imgAnimated);
             image.attr("state", "still");
 
-            var div = $("<div>");
+            var button = $("<button>").text("+").addClass("add-favorite");
+            button.attr("style", "float: right");
+
+            var div = $("<div>").addClass("img-wrapper");
             div.attr("style", "margin: 10px; float: left;")
-            div.append(p, image);
+            div.append(button,p, image);
             $("#image-container").prepend(div);
         }
     })
+}
+
+// Shows favorites on the page based on the passed list of image URLs.
+function updateFavorites(list)
+{
+    $("#favorites").empty(); 
+    for (var i = 0; i < list.length; i++) {
+      var favImage = $("<img>").attr("src", list[i]).addClass("fav-img");
+      favImage.attr("style", "width: 100%; height: 100%;");
+
+      var btnDelete = $("<button>").text("X");
+      btnDelete.attr("delete", i);
+      btnDelete.addClass("btn-delete");
+      btnDelete.attr("style", "position: absolute; top: 0px; right: 0px;");
+
+      var newDiv = $("<div>").attr("style", "position: relative;");
+      newDiv.append(favImage, btnDelete);
+      $("#favorites").prepend(newDiv);
+    }
 }
